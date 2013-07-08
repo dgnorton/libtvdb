db6c41e9051d49113a3cd4c0b4bd2312444e1630
David Norton <dgnorton@quoininc.com>
2013-07-08 09:49:11 -0400

    Fixed calloc of wrong size in tvdb_types.c.

--- src/tvdb_types.c
+++ src/tvdb_types.c
@@ -16,7 +16,7 @@ tvdb_series_t *tvdb_alloc_series() {
 
 tvdb_buffer_t *tvdb_alloc_buffer() {
    tvdb_buffer_t *buf;
-   buf = (tvdb_buffer_t *)calloc(sizeof(tvdb_series_t), 1);
+   buf = (tvdb_buffer_t *)calloc(sizeof(tvdb_buffer_t), 1);
    buf->memory = (char*)calloc(1, 1); 
    buf->size = 0;
    return buf;
